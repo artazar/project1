9f57bd370ef9033464a5706e332e37168d575371
mmbelyaev <gotfet420@gmail.com>
2019-07-04 11:17:32 +0700

    Script installs the following set of tools to the slave-box: 	-docker and docker-compose 	-Vagrant of version 2.2.5 	-Some default version of Virtualbox
    This script also creates a 'jenkins' system user with a home directory in /home/jenkins - this should be stated while adding the slave node

---
+++ slave_node_pre-script.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+USER=jenkins
+sudo apt-get install docker docker-compose -y
+wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.deb
+sudo install ./vagrant_2.2.5_x86_64.deb -y
+sudo apt-get install virtualbox -y
+sudo adduser --system --home /home/${USER} --shell /bin/bash --ingroup docker ${USER}
+sudo mkdir -p /home/${USER}/.ssh/
+sudo chown ${USER} /home/${USER}/.ssh
+sudo echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3Xoaz78I7AmvoJkUiydOzifTduCX8Secp0iWokJLBx' >> /home/${USER}/.ssh/authorized_keys
+sudo chown ${USER} home/${USER}/.ssh/authorized_keys
